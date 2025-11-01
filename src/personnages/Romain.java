ead836bedae5d257ec6a7956cc4550c66dbca8bf
Hadiltgit <hadil.takeznount@univ-tlse3.fr>
2025-11-01 23:40:05 +0100

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,5 +1,5 @@
 package personnages;
-
+import objets.*;
 public class Romain {
 
 	private String nom;
@@ -49,8 +49,44 @@ public class Romain {
 		}	
 	}
 	
+	Equipement[] equipements = new Equipement[2];
+	int nbEquipement=0;
+	
+	public void sEquiper(Equipement equipement) {
+	    switch (nbEquipement) {
+	        case 2:
+	            System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+	            break;
+
+	        case 1:
+	            if (equipements[0] == equipement) {
+	                System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+	            } else {
+	                equipements[nbEquipement] = equipement;
+	                nbEquipement++;
+	                System.out.println("Le soldat " + nom + " s’équipe avec un " + equipement + ".");
+	            }
+	            break;
+
+	        default: 
+	            equipements[nbEquipement] = equipement;
+	            nbEquipement++;
+	            System.out.println("Le soldat " + nom + " s’équipe avec un " + equipement + ".");
+	            break;
+	    }
+	}
+
+	
 	public static void main (String[] args) {
 		Romain minus = new Romain ("minus",6);
+		System.out.println(Equipement.CASQUE);
+		System.out.println(Equipement.BOUCLIER);
+		
+		minus.sEquiper(Equipement.CASQUE);
+	    minus.sEquiper(Equipement.CASQUE);
+	    minus.sEquiper(Equipement.BOUCLIER);
+	    minus.sEquiper(Equipement.CASQUE);
+		
 	}
 	
 }
